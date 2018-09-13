74c699de3d97312415109a74ddb4feab2811a44d
Philipp.Schmidt <philipp-kevin-schmidt@web.de>
2018-09-13 09:18:58 +0200

    Training: Reflections

--- Reflections/Program.cs
+++ Reflections/Program.cs
@@ -12,15 +12,41 @@ namespace Reflections
         static void Main(string[] args)
         {
             var assembly = Assembly.GetExecutingAssembly();
-            //Console.WriteLine(assembly);
+            //stored under C:\\windows\assembly
+            //Public / Shared assembly is stored in GAC Global Assembly Cache
 
-            foreach (var item in collection)
+            foreach (var type in assembly.GetTypes())
             {
+                Console.WriteLine("Assembly Type: {0}", type);
 
+                // returns an array of props from current assembly
+                var properties = type.GetProperties();
+                var fields = type.GetFields();
+
+                foreach (var property in properties)
+                {
+                    Console.WriteLine("Propertie: {0}", properties);
+                }
+                Console.WriteLine();
+
+                foreach (var field in fields)
+                {
+                    Console.WriteLine("Field: {0}",field);
+                }
+                
             }
 
             Console.ReadLine();
-
         }
+    }
+    public class anyClass
+    {
+        //that is a Field
+        public int Age;
+        //compiler creates automatically creates a private member of that type
+
+        //that is a propertie
+        public bool IsAdult { get; set; }
+
     }
 }
