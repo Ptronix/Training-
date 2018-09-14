bcb23bac11538a8ec431ea02e61468c7785a097d
Philipp.Schmidt <philipp-kevin-schmidt@web.de>
2018-09-14 15:44:38 +0200

    added custom events

--- Events/Program.cs
+++ Events/Program.cs
@@ -12,9 +12,13 @@ namespace Events
         static void Main(string[] args)
         {
             var tower = new ClockTower();
+
             var philipp = new Person("Philipp",tower);
 
+            Person Susi = new Person("Susi", tower);
+
             tower.ChimeFivePM();
+            tower.ChimeSixAM();
 
             Console.ReadLine();
 
@@ -32,24 +36,42 @@ namespace Events
                 _tower = tower;
 
                 //in parenthis must be the 2 arguments from the delegate
-                _tower.Chime += (object sender, EventArgs args) => Console.WriteLine("{0} heard the clock chime.",_name);
+                _tower.Chime += (object sender, ClockTowerEventArgs args) =>
+                {
+                    Console.WriteLine("{0} heard the clock chime.", _name);
+                    switch (args.Time)
+                    {
+                        case 6:
+                            Console.WriteLine("{0} is waking up", _name);
+                            break;
+                        case 17:
+                            Console.WriteLine("{0} is going home.",_name );
+                            break;
+                    }
+                };
             }
         }
+
+        public class ClockTowerEventArgs : EventArgs
+        {
+            public int Time { get; set;}
+        }
          
-        public delegate void ChimeEventHandler(object sender, EventArgs args);
+        public delegate void ChimeEventHandler(object sender, ClockTowerEventArgs args);
 
         public class ClockTower
         {
             public event ChimeEventHandler Chime;
 
+            //event with param
             public void ChimeFivePM()
             {
-                Chime(this, EventArgs.Empty);
+                Chime(this, new ClockTowerEventArgs { Time = 17 });
             }
 
             public void ChimeSixAM()
             {
-                Chime(this, EventArgs.Empty);
+                Chime(this, new ClockTowerEventArgs { Time = 6 });
             }
         }
     }
