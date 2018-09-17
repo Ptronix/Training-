985d78d9186843ab3988f457b2fd63ff04dcd851
Philipp.Schmidt <philipp-kevin-schmidt@web.de>
2018-09-17 10:09:16 +0200

    Example 1: item key item count

---
+++ LINQ/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sample = "I enjoy learning C#. Become a good programmer is hard work and you have to read a lot of.";
+
+            var result = from c in sample.ToLower()
+                         where c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+                         group c by c;
+
+
+            foreach (var item in result)
+            {
+                Console.WriteLine("{0} - {1}",item.Key,item.Count());
+            }
+            Console.ReadLine();
+        }
+    }
+}
