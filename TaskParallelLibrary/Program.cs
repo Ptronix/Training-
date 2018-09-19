8f4e1f8f818c9eaf6acc5ac17b11f54fe9c8e37b
Philipp.Schmidt <philipp-kevin-schmidt@web.de>
2018-09-19 16:30:55 +0200

    multithreading oldschool

---
+++ TaskParallelLibrary/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TaskParallelLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            //create task
+
+            var t1 = new Task(() => DoSomeStuff(1, 1500) );
+            t1.Start();
+            var t2 = new Task(() => DoSomeStuff(2, 500));
+            t2.Start();
+            var t3 = new Task(() => DoSomeStuff(3, 2000));
+            t3.Start();
+           
+
+            Console.WriteLine("Press any key to quit");
+            Console.ReadKey();
+        }
+        static void DoSomeStuff(int id, int sleepTime)
+        {
+            Console.WriteLine("task: {0} is beginning",id);
+            Thread.Sleep(sleepTime);
+            Console.WriteLine("task {0} has completed", id);
+        }
+    }
+}
