c2868756eb1ed99a4ad4341dc8e644d4a3416039
Philipp.Schmidt <philipp-kevin-schmidt@web.de>
2018-09-20 09:41:06 +0200

    Cpu 100% tester

--- TaskParallelLibrary/Program.cs
+++ TaskParallelLibrary/Program.cs
@@ -11,25 +11,18 @@ namespace TaskParallelLibrary
     {
         static void Main(string[] args)
         {
+            var intList = new List<long> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 33, 5, 11, 66, 44, 75 };
+
             Console.ForegroundColor = ConsoleColor.Green;
-            //create task and start the DoMoreStuff Task when prev task is finsish
-            //Chaining of Tasks
-            var t1 = Task.Factory.StartNew(() => DoSomeStuff(1, 1500)).ContinueWith((prevtask) => DoMoreStuff(1, 100));
+            Parallel.For(0, 9223372036854775807, (i) => Console.Write(i));
             
-            var t2 = Task.Factory.StartNew(() => DoSomeStuff(2, 3000)).ContinueWith((prevtask) => DoMoreStuff(2, 100));
-
-            var t3 = Task.Factory.StartNew(() => DoSomeStuff(3, 1000)).ContinueWith((prevtask) => DoMoreStuff(3, 100));
-
-            var taskList = new List<Task> { t1, t2, t3 };
-            Task.WaitAll(taskList.ToArray());
-
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Press any key to quit");
             Console.ReadKey();
         }
         static void DoSomeStuff(int id, int sleepTime)
         {
-            Console.ForegroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("task: {0} is beginning ",id);
             Thread.Sleep(sleepTime);
             Console.ForegroundColor = ConsoleColor.Red;
